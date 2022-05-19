2a59c005c9a6488ff14713ccff3b3ad1822967d1
venkat-dumpala <105343053+venkat-dumpala@users.noreply.github.com>
2022-05-19 22:41:35 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -1,8 +1,8 @@
 #include "stats.h"
 
-struct Stats compute_statistics(const float* numberset, int setlength) {   
+struct Stats compute_statistics(const float* numberset, int setlength) { }s;  
 
-    struct Stats s;
+ //   struct Stats s;
     s.average = 0;
     s.min = 0;
     s.max = 0;
