a1f31e8d92a36907266f7dfc1cd3fb3509cb8102
venkat-dumpala <105343053+venkat-dumpala@users.noreply.github.com>
2022-05-19 21:22:55 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -9,23 +9,24 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
 
 int emailAlertCallCount = 0;
 int ledAlertCallCount = 0;
-float max = 0;
+float max1 = 0;
 float sum = 0;
 float avg = 0;
-float min = 340282346638528859811704183484516925440;
+float min1 = 340282346638528859811704183484516925440;
 
 for(int i = 0; i< setlength ; i++;)
 {
     sum = sum + numberset[i];
-    if (numberset[i] > max)
-        max = vnumberset[i];
-    if (numberset[i] < min)
-        min = vnumberset[i];
+    if (numberset[i] > max1)
+        max1 = vnumberset[i];
+    if (numberset[i] < min1)
+        min1 = vnumberset[i];
 }
 
 avg = (float)(sum/setlength);
 
     s.average = avg;
-    s.min = min;
-    s.max = max;
-    
+    s.min = min1;
+    s.max = max1;
+
+return  s ;    
