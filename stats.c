a65788d0cf267e7435d5dcb07e4e2a47733e2893
venkat-dumpala <105343053+venkat-dumpala@users.noreply.github.com>
2022-05-19 21:38:48 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -1,14 +1,17 @@
 #include "stats.h"
 
+
+
+struct Stats compute_statistics(const float* numberset, int setlength) {   
+}s;
+
+
 int main()
 {
-
-struct Stats compute_statistics(const float* numberset, int setlength) {
-    struct Stats s;
+    
     s.average = 0;
     s.min = 0;
     s.max = 0;
-}
 
 int emailAlertCallCount = 0;
 int ledAlertCallCount = 0;
